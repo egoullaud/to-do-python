f9e48004a516dd926c925a9967b66311beda720d
elizabeth.goullaud <egoullaud@gmail.com>
2023-04-15 13:22:56 -0700

    functionality added

--- do-thing/main.py
+++ do-thing/main.py
@@ -43,39 +43,78 @@ app.run()
 # create application window
 app = gp.GooeyPieApp("Do it")
 app.width = 500
-app.set_grid(5, 3)
+app.set_grid(7, 3)
 
 # create functions
+#
+
+
+def add_task(event):
+    # add new task when enter is pressed
+    if event.key['name'] == 'Return':
+        list_box.add_item(new_task_input.text)
+        new_task_input.clear()
+        result_add_lbl.text = "As you wish, my liege"
+
+
+def add_task_btn(event):
+    list_box.add_item(new_task_input.text)
+    new_task_input.clear()
+    result_add_lbl.text = "As you wish, my liege"
+
+
+def delete_task(event):
+    list_box.remove_selected()
+
+
+def one_done(event):
+    if event.widget == list_box:
+        finished_box.add_item(list_box.remove_selected())
+
+
+def clear_all(event):
+    finished_box.items = []
+    clear_result_lbl.text = "My god, man. You've killed them all!"
 
 
 # create widgets
 new_task_lbl = gp.Label(app, "What needs doing?")
 new_task_input = gp.Input(app)
-new_task_btn = gp.Button(app, "Put it on the board", None)
+new_task_btn = gp.Button(app, "Put it on the board", add_task_btn)
+result_add_lbl = gp.Label(app, "")
 
-list_lbl = gp.Label(app, "List")
+list_lbl = gp.Label(app, "Live List")
 list_box = gp.Listbox(app)
-delete_btn = gp.Button(app, "Not doing it", None)
+delete_btn = gp.Button(app, "Not doing it", delete_task)
 all_done_btn = gp.Button(app, "Did all the things", None)
 
 
 finished_label = gp.Label(app, "Kill List")
 finished_box = gp.Listbox(app)
-clear_btn = gp.Button(app, "Clear Killed", None)
+clear_btn = gp.Button(app, "Clear Killed", clear_all)
+clear_result_lbl = gp.Label(app, "")
+
+
+# Set up Events
+new_task_input.add_event_listener('key_press', add_task)
+list_box.add_event_listener('double_click', one_done)
+
 
 # place widgets on grid
 app.add(new_task_lbl, 1, 1, align="right")
-app.add(new_task_input, 1, 2, align="left")
+app.add(new_task_input, 1, 2,  align="left", fill=True, stretch=True)
 app.add(new_task_btn, 1, 3, align="center")
+app.add(result_add_lbl, 2, 2, align='center')
 
-app.add(list_lbl, 2, 1, align="right")
-app.add(list_box, 2, 2, align='left')
-app.add(delete_btn, 3, 2, align='center')
-app.add(all_done_btn, 3, 3, align='center')
+app.add(list_lbl, 3, 1, align="right")
+app.add(list_box, 3, 2, column_span=2, fill=True, stretch=True)
+app.add(delete_btn, 4, 2, align='center')
+app.add(all_done_btn, 4, 3, align='center')
 
-app.add(finished_label, 4, 1, align='right')
-app.add(finished_box, 4, 2, align='left')
-app.add(clear_btn, 5, 3, align='center')
+app.add(finished_label, 5, 1, align='right')
+app.add(finished_box, 5, 2, column_span=2, fill=True, stretch=True)
+app.add(clear_btn, 6, 3, align='center')
+app.add(clear_result_lbl, 6, 2, align='center')
 
 
 # run app
